1d930c685597c3eb50cea4ac444fb9691e66779c
Holly Hobson <holly.ashhurst@gmail.com>
2025-05-30 18:03:48 +1000

    Connect to local mySQL

--- airbnb-project/backend/db.js
+++ airbnb-project/backend/db.js
@@ -1,17 +1,37 @@
-const mysql = require('mysql2');
+"use strict";
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'your_user',
-  password: 'your_password',
-  database: 'your_db_name'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "", {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
 });
+const connectMysql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Successful connection to MySQL Database
+${process.env.DB_NAME}`);
+  } catch (error) {
+    console.error("Unable to connect to MySQL database:", error);
+    process.exit(1);
+  }
+};
+connectMysql();
+module.exports = {
+  Sequelize: sequelize,
+};
 
-module.exports = pool.promise();
 
+// const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'your_user',
+//   password: 'your_password',
+//   database: 'your_db_name'
+// });
 
-
+// module.exports = pool.promise();
 
 
 
@@ -28,11 +48,6 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;*/
-
-
-
-
-
 
 
 /*const mysql = require('mysql2');
